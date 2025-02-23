dce3d3ac98ea31b4f009125fdcbbf366b972257a
ridoyexpressitbd <ridoy@expressitbd.dev>
2025-02-23 04:03:15 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ['https://expressitbd-frontend-task.vercel.app'],
+    origin: ["https://expressitbd-frontend-task.vercel.app", "*"],
     credentials: true
   })
 )
