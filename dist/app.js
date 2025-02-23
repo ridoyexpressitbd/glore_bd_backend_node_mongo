6b761c56e17a616eb845b4937e9f309147e3f68c
ridoyexpressitbd <ridoy@expressitbd.dev>
2025-02-23 15:14:28 -0500

    set cors origin

--- dist/app.js
+++ dist/app.js
@@ -13,12 +13,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: [
-        'http://localhost:5173',
-        'http://localhost:3000',
-        'https://glore-bd-frontend-node-mongo.vercel.app',
-        'https://new.glorebd.com'
-    ],
+    origin: '*',
     credentials: true
 }));
 app.use('/api', routers_1.default);
